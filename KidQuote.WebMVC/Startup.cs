e14e97f408458479190d53b4d6dfc24fdc418282
Paul O'Connor <jamespauloconnor@gmail.com>
2017-12-04 21:53:23 -0500

    Add project files.

---
+++ KidQuote.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KidQuote.WebMVC.Startup))]
+namespace KidQuote.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
