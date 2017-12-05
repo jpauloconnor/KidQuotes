e98306f7108644bce45068c559a23461ceb7775d
Paul O'Connor <jamespauloconnor@gmail.com>
2017-12-04 23:42:13 -0500

    Added Interface

---
+++ KidQutoes.Contracts/IQuote.cs
@@ -0,0 +1,20 @@
+﻿using KidQuotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidQutoes.Contracts
+{
+    public interface IQuote
+    {
+        public bool CreateQuote(QuoteCreateModel model);
+        public IEnumerable<NoteListItem> GetNotes();
+        public NoteDetail GetNoteById(int noteId);
+        public bool UpdateNote(NoteEdit model);
+        public bool DeleteNote(int noteId);
+
+
+    }
+}
