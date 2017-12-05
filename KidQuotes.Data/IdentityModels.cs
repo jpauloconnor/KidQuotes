fa4406ac9ec508cf581436c0cc15a6c2fb972914
Paul O'Connor <jamespauloconnor@gmail.com>
2017-12-05 09:34:04 -0500

    Added CreateQuote Service method and constructor. Fixed IdentityModels namespace.

--- KidQuotes.Data/IdentityModels.cs
+++ KidQuotes.Data/IdentityModels.cs
@@ -7,9 +7,8 @@ using KidQuotes.Data;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Data.Entity.ModelConfiguration;
 
-namespace KidQuotes.WebMVC.Models
+namespace KidQuotes.Data
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
--- KidQuotes.Services/QuoteService.cs
+++ KidQuotes.Services/QuoteService.cs
@@ -5,15 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using KidQuotes.Models;
+using KidQuotes.Data;
 
 namespace KidQuotes.Services
 {
     public class QuoteService : IQuote
     {
+        private readonly Guid _userId;
+
+        public QuoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
         public bool CreateQuote(QuoteCreateModel model)
         {
-            throw new NotImplementedException();
+            var entity =
+                new QuoteEntity
+                {
+                    OwnerId = _userId,
+                    Quote = model.Quote,
+                    Description = model.Description,
+                    CreatedUtc = DateTimeOffset.UtcNow
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Quotes.Add(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
         }
+       
 
         public bool DeleteQuote(int quoteId)
         {
