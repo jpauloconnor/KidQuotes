69bed5c280e8c927d126bc8cc072f10e7c0de780
Paul O'Connor <jamespauloconnor@gmail.com>
2018-01-06 09:00:31 -0500

    Book sales controller set up

---
+++ KidQuotes.Models/BookPurchaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidQuotes.Models
+{
+    public class BookPurchaseViewModel
+    {
+        public string StripePublishableKey { get; set; }
+    }
+}
---
+++ KidQuotes.WebMVC/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using KidQuotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidQuotes.WebMVC.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            string stripePublishableKey = ConfigurationManager.AppSettings["stripePublishableKey"];
+            var model = new BookPurchaseViewModel() { StripePublishableKey = stripePublishableKey };
+            return View(model);
+        }
+    }
+}
